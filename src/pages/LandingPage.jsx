ec5ebbb91a62c964fb8e7410ba0eb0318ff2a1dd
Yoni <yoniakabecky@gmail.com>
2020-04-21 22:09:20 -0700

    Modified Hero Section

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,56 +1,22 @@
 import React from "react";
 
 // Mui
-import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 
 // Components
-import LandingLeftPane from "../components/home/LandingLeftPane";
-import LandingRightPane from "../components/home/LandingRightPane";
+import Hero from "../components/Landing/Hero";
+import { Container } from "@material-ui/core";
 
 const LandingPage = () => {
   const classes = useStyles();
 
   return (
-    <Grid container component="main" className={classes.root}>
-      <Grid
-        item
-        xs={12}
-        sm={8}
-        md={5}
-        elevation={6}
-        className={classes.contents}
-      >
-        <LandingLeftPane />
-      </Grid>
-      <Grid item xs={false} sm={4} md={7}>
-        <LandingRightPane />
-      </Grid>
-    </Grid>
+    <main>
+      <Hero />
+    </main>
   );
 };
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    height: "calc(100vh - 64px)"
-  },
-  contents: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    textAlign: "center"
-  },
-  btn: {
-    margin: "4rem 0",
-    width: "10rem",
-    height: "3rem",
-    marginTop: "5rem"
-  },
-  link: {
-    color: "inherit",
-    textDecoration: "none"
-  }
-}));
+const useStyles = makeStyles((theme) => ({}));
 
 export default LandingPage;
